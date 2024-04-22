589efec64c36a02e2b90561b1018398644c0598e
Attaneva21 <attaneva21@codingburgas.bg>
2024-04-22 08:48:22 +0300

    Fix main menu font

--- The lore trove/Library/Renderer.cpp
+++ The lore trove/Library/Renderer.cpp
@@ -47,11 +47,11 @@ void Renderer::Update() {
 	if (menu) {
 		if (main) {  // Render main menu options
 			DrawTexture(menuBackground, 0, 0, WHITE);
-			DrawText("The lore trove", 30, 150, 70, WHITE);
-			DrawText("Play", button.mainMenuButtons[0].x + 60, button.mainMenuButtons[0].y - 4, 70, WHITE);
-			DrawText("Info", button.mainMenuButtons[1].x + 60, button.mainMenuButtons[1].y - 4, 70, WHITE);
-			DrawText("Credits", button.mainMenuButtons[2].x + 60, button.mainMenuButtons[2].y - 4, 70, WHITE);
-			DrawText("Exit", button.mainMenuButtons[3].x + 60, button.mainMenuButtons[3].y - 4, 70, WHITE);
+			DrawTextEx(font, "The lore trove", { 30, 150 }, 90,5, WHITE);
+			DrawTextEx(font, "Play", { button.mainMenuButtons[0].x + 60, button.mainMenuButtons[0].y - 4 }, 70,5, WHITE);
+			DrawTextEx(font, "Info", { button.mainMenuButtons[1].x + 60, button.mainMenuButtons[1].y - 4 }, 70, 5, WHITE);
+			DrawTextEx(font, "Credits", { button.mainMenuButtons[2].x + 60, button.mainMenuButtons[2].y - 4 }, 70, 5, WHITE);
+			DrawTextEx(font, "Quit", { button.mainMenuButtons[3].x + 60, button.mainMenuButtons[3].y - 4 }, 70, 5, WHITE);
 
 			// Check for button clicks
 			if (button.IsClicked(button.mainMenuButtons[0])) {
